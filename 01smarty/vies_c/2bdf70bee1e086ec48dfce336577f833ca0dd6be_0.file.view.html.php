19d6d3f70b932ace5af9f79eee3841854f2ca08a
rask <2083967667@qq.com>
2018-07-05 19:37:09 +0800

    指定了samrty的分隔符和视图及混编文件路径

---
+++ 01smarty/vies_c/2bdf70bee1e086ec48dfce336577f833ca0dd6be_0.file.view.html.php
@@ -0,0 +1,39 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-07-05 11:35:00
+  from 'D:\wamp64\www\smarty3132\01smarty\views\view.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5b3e0264d746c9_65848132',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2bdf70bee1e086ec48dfce336577f833ca0dd6be' => 
+    array (
+      0 => 'D:\\wamp64\\www\\smarty3132\\01smarty\\views\\view.html',
+      1 => 1530790452,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5b3e0264d746c9_65848132 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>view</title>
+</head>
+<body>
+
+<h1><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+</h1>
+<p><?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+</p>
+
+</body>
+</html><?php }
+}
