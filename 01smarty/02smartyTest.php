379562d812769c789c6da6f0eba8ede329f6620a
rask <2083967667@qq.com>
2018-07-05 20:27:08 +0800

    常量，配置文件

--- 01smarty/02smartyTest.php
+++ 01smarty/02smartyTest.php
@@ -4,9 +4,7 @@ require "libs/Smarty.class.php";
 
 //实例化samrty
 $smarty=new Smarty();
-//指定解析符号:{}和样式表的符号冲突。样式表的{}添加空格和自动换行后就不会冲突。
-$smarty->setLeftDelimiter("<{");
-$smarty->setRightDelimiter("}>");
+
 //指定模板目录
 $smarty->setTemplateDir("./views/");
 //指定混编文件路径
@@ -15,6 +13,11 @@ $smarty->setCompileDir("./views_c/");
 //传值
 $smarty->assign("title","白头吟");
 $smarty->assign("content","愿得一人心，白首不分离！");
+$smarty->assign("name",$_GET['name']);
+
+//常量也是全局常量
+define("PI",3.1415926);
+
 
 //生成混编文件
-$smarty->display("view.html");
+$smarty->display("view2.html");
--- 01smarty/03smartyTest.php
+++ 01smarty/03smartyTest.php
@@ -1,12 +1,12 @@
 <?php
+//配置文件和配置变量
+
 //引入smarty类
 require "libs/Smarty.class.php";
 
 //实例化samrty
 $smarty=new Smarty();
-//指定解析符号:{}和样式表的符号冲突。样式表的{}添加空格和自动换行后就不会冲突。
-$smarty->setLeftDelimiter("<{");
-$smarty->setRightDelimiter("}>");
+
 //指定模板目录
 $smarty->setTemplateDir("./views/");
 //指定混编文件路径
@@ -16,5 +16,6 @@ $smarty->setCompileDir("./views_c/");
 $smarty->assign("title","白头吟");
 $smarty->assign("content","愿得一人心，白首不分离！");
 
+
 //生成混编文件
-$smarty->display("view.html");
+$smarty->display("view3.html");
